02da0b7bc924b87b39227d82c4822f8f43374528
deepanshu <deepanshu2912001@gmail.com>
2026-01-25 19:06:39 +0530

    add project and blog page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export interface ProjectCardProps {
+  title: string;
+  dates: string;
+  sourceUrl?: string;
+  description: string;
+  imageUrl: string;
+  projectUrl: string;
+  technologies: string[];
+}
+
+export interface BlogCardProps {
+  title: string;
+  pubdate: string;
+  readtime: string;
+  description: string;
+  imageUrl?: string;
+  projectUrl?: string;
+  tags?: string[];
+}
